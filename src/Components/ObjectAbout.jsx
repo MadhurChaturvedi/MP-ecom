e7c75d916c49cb4d86e2232a66bdd9b8f7a7885d
madhur <madhurchaturvedi2000@gmail.com>
2025-06-05 00:34:44 +0530

    add class base component

---
+++ src/Components/ObjectAbout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class objectAbout extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>About</h1>
+            </div>
+        )
+    }
+}
+
+export default objectAbout;
